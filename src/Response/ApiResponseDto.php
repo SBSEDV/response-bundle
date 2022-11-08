fe653b4538757a9051caf3f1a06b44824fd5a72e
Daniel Burger <48986191+danielburger1337@users.noreply.github.com>
2022-11-08 10:27:47 +0100

    Add ApiResponseDto

---
+++ src/Response/ApiResponseDto.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace SBSEDV\Bundle\ResponseBundle\Response;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\Translation\TranslatableInterface;
+
+class ApiResponseDto
+{
+    /**
+     * @param TranslatableInterface|string|null $message The "message" property.
+     * @param mixed                             $data    The "data" property.
+     * @param ApiResponseErrorDto[]             $errors  The "errors" property.
+     * @param int                               $status  The "status" property.
+     */
+    public function __construct(
+        public readonly TranslatableInterface|string|null $message = null,
+        public readonly mixed $data = null,
+        public readonly array $errors = [],
+        public readonly int $status = Response::HTTP_OK
+    ) {
+    }
+}
